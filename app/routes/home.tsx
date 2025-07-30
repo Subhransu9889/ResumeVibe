2f015bcd14bda7021068714509651d0ef75d14f6
Subransu <maharanasubhransu6@gmail.com>
2025-07-30 20:29:20 +0530

    Implementing the resume card and home page ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeVibe" },
+    { name: "description", content: "Instantly analyze and enhance your resume for maximum impact.!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
+    <Navbar/>
+    <section className='main-section'>
+      <div className='page-heading py-16'>
+        <h1>Track Your Applications & Resume Rating.</h1>
+        <h2>Review your submissions and chek AI-powered feedback.</h2>
+      </div>
+    {
+      resumes.length>0 && (
+          <div className='resumes-section'>
+            {
+              resumes.map((resume) => (
+                  <div>
+                    <ResumeCard key={resume.id} resume={resume}/>
+                  </div>
+              ))
+            }
+          </div>
+        )
+    }
+    </section>
+  </main>;
 }
